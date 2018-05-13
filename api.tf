7c6731b1e1e5cfa65484c71f47178e9b4dcae005
John DiStasio <jndistasio@gmail.com>
2018-05-13 01:44:09 -0400

    Implement pdf service

--- api.tf
+++ api.tf
@@ -48,7 +48,8 @@ resource "aws_lambda_function" "hello" {
 }
 
 resource "aws_api_gateway_rest_api" "hello" {
-  name = "Hello"
+  name               = "Hello"
+  binary_media_types = ["application/pdf"]
 }
 
 resource "aws_api_gateway_resource" "hello" {
@@ -76,6 +77,27 @@ resource "aws_api_gateway_integration" "hello" {
 
   type = "AWS_PROXY"
   uri  = "arn:aws:apigateway:${data.aws_region.current.name}:lambda:path/2015-03-31/functions/${aws_lambda_function.hello.arn}/invocations"
+}
+
+resource "aws_api_gateway_method_response" "hello" {
+  rest_api_id = "${aws_api_gateway_rest_api.hello.id}"
+  resource_id = "${aws_api_gateway_resource.hello.id}"
+  http_method = "${aws_api_gateway_method.hello.http_method}"
+  status_code = "201"
+
+  # This is necessary to avoid a race condition
+  depends_on = ["aws_api_gateway_method.hello"]
+}
+
+resource "aws_api_gateway_integration_response" "hello" {
+  rest_api_id      = "${aws_api_gateway_rest_api.hello.id}"
+  resource_id      = "${aws_api_gateway_resource.hello.id}"
+  http_method      = "${aws_api_gateway_method.hello.http_method}"
+  status_code      = "201"
+  content_handling = "CONVERT_TO_BINARY"
+
+  # This is necessary to avoid a race condition
+  depends_on = ["aws_api_gateway_integration.hello"]
 }
 
 data "aws_caller_identity" "current" {}
