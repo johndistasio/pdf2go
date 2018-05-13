f5434fdfa5cee5e90428a2671199b9cdf256c4da
John DiStasio <jndistasio@gmail.com>
2018-05-13 02:04:36 -0400

    Adjust dependencies in API Gateway resources for a clean build

--- api.tf
+++ api.tf
@@ -79,22 +79,22 @@ resource "aws_api_gateway_integration" "hello" {
   uri  = "arn:aws:apigateway:${data.aws_region.current.name}:lambda:path/2015-03-31/functions/${aws_lambda_function.hello.arn}/invocations"
 }
 
-resource "aws_api_gateway_method_response" "hello" {
-  rest_api_id = "${aws_api_gateway_rest_api.hello.id}"
-  resource_id = "${aws_api_gateway_resource.hello.id}"
-  http_method = "${aws_api_gateway_method.hello.http_method}"
-  status_code = "201"
-
-  # This is necessary to avoid a race condition
-  depends_on = ["aws_api_gateway_method.hello"]
-}
-
-resource "aws_api_gateway_integration_response" "hello" {
+resource "aws_api_gateway_integration_response" "201" {
   rest_api_id      = "${aws_api_gateway_rest_api.hello.id}"
   resource_id      = "${aws_api_gateway_resource.hello.id}"
   http_method      = "${aws_api_gateway_method.hello.http_method}"
   status_code      = "201"
   content_handling = "CONVERT_TO_BINARY"
+
+  # This is necessary to avoid a race condition
+  depends_on = ["aws_api_gateway_integration.hello"]
+}
+
+resource "aws_api_gateway_method_response" "201" {
+  rest_api_id = "${aws_api_gateway_rest_api.hello.id}"
+  resource_id = "${aws_api_gateway_resource.hello.id}"
+  http_method = "${aws_api_gateway_method.hello.http_method}"
+  status_code = "201"
 
   # This is necessary to avoid a race condition
   depends_on = ["aws_api_gateway_integration.hello"]
